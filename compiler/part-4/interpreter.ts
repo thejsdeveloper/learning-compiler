7fbcd2a989c1f0a7491cf47b4fcbcddecc982727
thejsdeveloper <thejsdeveloper@gmail.com>
2020-02-26 00:01:16 +0800

    1 file modified

--- compiler/part-4/interpreter.ts
+++ compiler/part-4/interpreter.ts
@@ -1,40 +1,48 @@
 import { Lexer } from "./lexer";
 import { Token, TOKEN_TYPE } from "../model/token";
 
-
-
 export class Interpreter {
   currentToken: Token;
   constructor(private lexer: Lexer) {
     this.currentToken = this.lexer.getNextToken();
   }
 
-
   expr() {
-    debugger
+    debugger;
     let result = this.factor();
 
-    while( 
-      ( this.currentToken.tokenType === TOKEN_TYPE.MUL ||
-      this.currentToken.tokenType === TOKEN_TYPE.DIV )
-      ) {
-        const token = this.currentToken;
+    while (
+      this.currentToken.tokenType === TOKEN_TYPE.PLUS ||
+      this.currentToken.tokenType === TOKEN_TYPE.MINUS ||
+      this.currentToken.tokenType === TOKEN_TYPE.MUL ||
+      this.currentToken.tokenType === TOKEN_TYPE.DIV
+    ) {
+      const token = this.currentToken;
 
-        if (token.tokenType === TOKEN_TYPE.MUL) {
-          this.eat(TOKEN_TYPE.MUL);
-          result = result * this.factor();
-        }
+      if (token.tokenType === TOKEN_TYPE.PLUS) {
+        this.eat(TOKEN_TYPE.PLUS);
+        result = result + this.factor();
+      }
 
-        if (token.tokenType === TOKEN_TYPE.DIV) {
-          this.eat(TOKEN_TYPE.DIV);
-          result = result / this.factor();
-        }
+      if (token.tokenType === TOKEN_TYPE.MINUS) {
+        this.eat(TOKEN_TYPE.MINUS);
+        result = result  - this.factor();
+      }
+      if (token.tokenType === TOKEN_TYPE.MUL) {
+        this.eat(TOKEN_TYPE.MUL);
+        result = result * this.factor();
+      }
+
+      if (token.tokenType === TOKEN_TYPE.DIV) {
+        this.eat(TOKEN_TYPE.DIV);
+        result = result / this.factor();
+      }
     }
     return result;
   }
 
   factor(): number {
-    const token =  this.currentToken;
+    const token = this.currentToken;
     this.eat(TOKEN_TYPE.INTEGER);
     return parseFloat(token.value);
   }
@@ -44,9 +52,8 @@ export class Interpreter {
 
     if (token.tokenType === tokenType) {
       this.currentToken = this.lexer.getNextToken();
-    }  else {
-      throw new Error('Error while parsing');
+    } else {
+      throw new Error("Error while parsing");
     }
-
   }
 }
