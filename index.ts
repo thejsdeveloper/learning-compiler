0c1ec3ba994dea6726971fcdb2789dac00322fa4
thejsdeveloper <thejsdeveloper@gmail.com>
2020-02-28 00:01:07 +0800

    adding it to index for testing

--- index.ts
+++ index.ts
@@ -5,18 +5,33 @@ import './style.css';
 // import {Interpreter} from './simple-add-parser';
 
 
-import { Lexer } from './compiler/lexer';
+// import { Lexer } from './compiler/lexer';
 import { Interpreter } from './compiler/part-5/interpreter';
+import { Lexer } from './compiler/part-6/lexer';
+import { Parser } from './compiler/part-6/parser';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 
 let str = '14 + 2 * 3 - 6 / 2';
-const lex = new Lexer(str);
+// const lex = new Lexer(str);
 
-const intr = new Interpreter(lex);
-const res = intr.expr();
-console.log(res);
+// const intr = new Interpreter(lex);
+// const res = intr.expr();
+// console.log(res);
+
+
+
+console.log('>>>>>>PART 7>>>>>>>>>>');
+
+const lex7 = new Lexer(str);
+
+const parser = new Parser(lex7);
+
+const ast = parser.parse();
+
+console.log('AST >>>>>', ast)
+
 
 
