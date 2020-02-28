945f932c34b5a5882f627ce678a7bd95dd07e866
thejsdeveloper <thejsdeveloper@gmail.com>
2020-02-28 15:57:02 +0800

    1 file modified

--- tree/draw-tree.ts
+++ tree/draw-tree.ts
@@ -1,12 +1,11 @@
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
 export class DrawTree {
-
   margin = {
     top: 20,
     right: 20,
     bottom: 20,
-    left: 20,
+    left: 20
   };
   width = 960;
   height = 500;
@@ -15,70 +14,71 @@ export class DrawTree {
   treeHeight = 500 - this.margin.top - this.margin.bottom;
 
   duration = 750;
-
+  svg = d3.select(".tree-container")
+    .append("svg")
+    .attr("height", this.height)
+    .attr("width", this.width)
+    .append("g")
+    .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`);
+    
+  root = {};
   constructor(private data: any) {
-
+   this.root = d3.hierarchy(this.data);
   }
 
-
   draw() {
+
+    this.update(this.root);
+  }
+
+  private update(source) {
+    
+    const duration = d3.event && d3.event.altKey ? 2500 : 250;
     const tree = d3.tree().size([this.treeWidth, this.treeHeight]);
 
+    const transition = this.svg.transition()
+      .duration(duration)
+
 
     
-    let root = d3.hierarchy(this.data);
-    console.log("TCL: DrawTree -> draw -> nodes", root)
-    
-    tree(root);
-    console.log("TCL: DrawTree -> draw -> nodes", root.descendants())
+    tree(this.root);
 
-    const links = root.descendants().slice(1);
-      console.log("TCL: DrawTree -> draw -> nodes", links)
+    const links = this.root.descendants().slice(1);
     const line = d3.line().curve(d3.curveBasis);
 
-
-
-    const svg = d3.select('.tree-container')
-      .append('svg')
-      .attr('height', this.height)
-      .attr('width', this.width)
-      .append('g')
-      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
-
-
-    svg.selectAll('.link')
+    this.svg
+      .selectAll(".link")
       .data(links)
       .enter()
-      .append('path')
-      .attr('fill', 'none')
-      .attr('stroke', 'lightblue')
-      .attr('d', (d: any) => {
+      .append("path")
+      .attr("fill", "none")
+      .attr("stroke", "lightblue")
+      .attr("d", (d: any) => {
         return line([
           [d.x, d.y],
           [d.x, (d.y + d.parent.y) / 2],
           [d.parent.x, (d.y + d.parent.y) / 2],
           [d.parent.x, d.parent.y]
-        ])
+        ]);
       });
 
-
-  svg.selectAll('.node')
-      .data(root.descendants())
+    const nodeEnter = this.svg
+      .selectAll(".node")
+      .data(this.root.descendants())
       .enter()
-      .append('g')
-      .attr('class', 'node')
-      .append('circle')
-      .attr('r', 4.5)
-      .attr('fill', '#4a4a4a')
-      .attr('cx', (d: any) => d.x)
-      .attr('cy', (d: any) => d.y);
-
-
-
+      .append("g")
+      .attr("class", "node")
+      .append("circle")
+      .attr("r", 20)
+      .attr("fill", "#fff")
+      .attr("stroke", "lightblue")
+      .attr("cx", (d: any) => d.x)
+      .attr("cy", (d: any) => d.y)
+      .on("click", (d: any) => {
+        console.log(d);
+        d.children = d.children ? null : d._children;
+        this.update(d)
+      });
   }
-
-
-
-
-
+    
 }
